801dbd5948bb642924eaa517ae735d433cc559a1
汤波 <478185912@qq.com>
2016-02-01 00:32:40 +0800

    修改部分样式

--- HWYZ/Scripts/Page/reportofsale.js
+++ HWYZ/Scripts/Page/reportofsale.js
@@ -40,6 +40,14 @@ $(function () {
                     text: '营业额'
                 }
             },
+            lang: {
+                printChart: "打印图表",
+                downloadJPEG: "下载JPEG 图片",
+                downloadPDF: "下载PDF文档",
+                downloadPNG: "下载PNG 图片",
+                downloadSVG: "下载SVG 矢量图",
+                exportButtonTitle: "导出图片"
+            },
             tooltip: {
                 formatter: function () {
                     return '<b>' + this.series.name + '</b><br/>' +
